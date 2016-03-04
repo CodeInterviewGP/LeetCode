e1d06840681c0e897470f1699620b08d56bcedb4
alaitang <tangalai@hotmail.com>
2016-03-03 16:48:31 -0800

    Commit question 286

---
+++ Solution/LeetCode/Questions/Q286/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Questions.Q286
+{
+    public class Solution
+    {
+        public void WallsAndGates(int[,] rooms)
+        {
+
+        }
+    }
+}
