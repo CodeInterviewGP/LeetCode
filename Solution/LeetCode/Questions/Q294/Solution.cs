c2dbac5477e28c26f172a9b59801a426e74338f8
alaitang <tangalai@hotmail.com>
2016-03-03 22:12:08 -0800

    Commit Q249 solution 1

--- Solution/LeetCode/Questions/Q294/Solution.cs
+++ Solution/LeetCode/Questions/Q294/Solution.cs
@@ -10,7 +10,42 @@ namespace LeetCode.Questions.Q294
     {
         public bool CanWin(string s)
         {
-            return true;
+
+            IList<string> nextSteps = GeneratePossibleNextMoves(s);
+
+
+            foreach (string ss in nextSteps)
+            {
+                if (!CanWin(ss))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+        public IList<string> GeneratePossibleNextMoves(string s)
+        {
+            IList<string> result = new List<string>();
+
+            int len = s.Length;
+
+            for (int i = 1; i < len; i++)
+            {
+                if (s[i] == '+' && s[i - 1] == '+')
+                {
+                    StringBuilder sb = new StringBuilder(s);
+
+                    sb[i] = '-';
+                    sb[i - 1] = '-';
+
+                    result.Add(sb.ToString());
+                }
+            }
+
+            return result;
         }
     }
 }
