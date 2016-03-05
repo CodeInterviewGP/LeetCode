e4374a26f0f9e04667141fa058693ccf829b58a9
alaitang <tangalai@hotmail.com>
2016-03-04 21:34:03 -0800

    Add Q6

---
+++ Solution/OtherQuestions/Questions/Q6/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherQuestions.Questions.Q6
+{
+    class Question
+    {
+        /*
+        Give a positive number N, find all possible positive numbers can be sum equals N
+        for example, N = 4, {[1,3],[3,1],[2,2],[1,1,1,1],[1,1,2],[1,2,1],[2,1,1]} , return 7;
+        N = 3 , {[1,1,1],[1,2],[2,1]} return 3;
+        N = 1 , {[1]} return 1;
+        
+        space complex required O(1)
+        */
+    }
+}
---
+++ Solution/OtherQuestions/Questions/Q6/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherQuestions.Questions.Q6
+{
+    public class Solution
+    {
+        public int Fun(int target)
+        {
+            return 1;
+        }
+    }
+}
