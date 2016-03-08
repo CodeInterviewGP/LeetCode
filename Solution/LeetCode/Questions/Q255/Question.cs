58478cc9678b4657d02e2e9cff53827948b5af84
alaitang <tangalai@hotmail.com>
2016-03-08 12:50:45 -0800

    Add leetcode 255

---
+++ Solution/LeetCode/Questions/Q255/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Questions.Q255
+{
+    class Question
+    {
+
+        /*
+        Given an array of numbers, verify whether it is the correct preorder traversal sequence of a binary search tree.
+
+            You may assume each number in the sequence is unique.
+
+            Follow up:
+            Could you do it using only constant space complexity?
+        */
+    }
+}
---
+++ Solution/LeetCode/Questions/Q255/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Questions.Q255
+{
+    public class Solution
+    {
+        public bool VerifyPreorder(int[] preorder)
+        {
+            return true;
+        }
+    }
+}
