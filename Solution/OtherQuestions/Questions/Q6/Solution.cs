9db7d45be9e60d1212675520a2c096631699afc2
alaitang <tangalai@hotmail.com>
2016-03-07 23:07:47 -0800

    commit Q6

--- Solution/OtherQuestions/Questions/Q6/Solution.cs
+++ Solution/OtherQuestions/Questions/Q6/Solution.cs
@@ -10,7 +10,12 @@ namespace OtherQuestions.Questions.Q6
     {
         public int Fun(int target)
         {
-            return 1;
+            if (target == 1)
+            {
+                return 1;
+            }
+
+            return (int)Math.Pow(2, target - 1) - 1;
         }
     }
 }
